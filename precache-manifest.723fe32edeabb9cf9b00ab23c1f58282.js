ffbbe39eb28c60d57f266588861473fdd5112abd
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-08-15 10:23:32 +0900

    Updates

---
+++ precache-manifest.723fe32edeabb9cf9b00ab23c1f58282.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff1bb0e0cdbd501472119045aa20dfe",
+    "url": "/react-rpg/index.html"
+  },
+  {
+    "revision": "fcf2099e8bea6eb1398b",
+    "url": "/react-rpg/static/js/2.6f265ba4.chunk.js"
+  },
+  {
+    "revision": "e354f2d257213cdb2b23",
+    "url": "/react-rpg/static/js/main.36597387.chunk.js"
+  },
+  {
+    "revision": "965d54c2106ddcfdf043",
+    "url": "/react-rpg/static/js/runtime~main.deedadac.js"
+  },
+  {
+    "revision": "c8ae0df8b009b3785d39b75cb4a5075a",
+    "url": "/react-rpg/static/media/bard.c8ae0df8.png"
+  }
+]);
